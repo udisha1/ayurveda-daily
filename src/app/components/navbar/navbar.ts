8a6a3398a86b4a5f3cf77caac37bb615cfe232db
bivekm01 <2303051051182@paruluniversity.ac.in>
2026-06-03 13:53:19 +0545

    Improve homepage UI and active navbar highlighting

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -9,7 +9,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
   isScrolled = false;
   menuOpen = false;

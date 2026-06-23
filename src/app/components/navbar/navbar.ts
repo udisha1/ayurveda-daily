59554cf6091e8b09e777e9a0b637750f646f3a5c
Chethana M <chethanamp22@gmail.com>
2026-06-23 23:03:39 +0530

    feat: Add navbar settings, audio player functionality, and footer improvements

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -23,8 +23,9 @@ export class NavbarComponent {
     this.healingMode = !this.healingMode;
     document.body.classList.toggle('healing-mode');
   }
-}
-}
+
   closeMenu() {
+    this.menuOpen = false;
   }
 }
+

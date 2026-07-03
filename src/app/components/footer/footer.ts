f47e37bd759732ff0848c9a1fff42daa9395e217
RAgini Singh <sengarragini15@gmail.com>
2026-07-04 00:45:02 +0530

    fix: make footer navigation links functional using Angular routerLink

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './footer.html',
   styleUrls: ['./footer.css']
 })
 export class FooterComponent {
-  year: number = new Date().getFullYear(); 
+  year = new Date().getFullYear();
 }
